db22084d3903558586a67f07748414e133b6dc1d
audiosupp <ivangerasimyukmail@gmail.com>
2024-07-17 14:55:30 +0300

    done

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -30,6 +30,11 @@ def peak_time_reg(regtime)
   hour
 end
 
+def peak_reg_day(regday)
+  regday = regday.split('/').map(&:to_i)
+  Date.new(regday[2], regday[0], regday[1]).wday
+end
+
 def legislators_by_zipcode(zipcode)
   civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
   civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
@@ -67,23 +72,27 @@ erb_template = ERB.new template_letter
 
 
 temp = []
+temp_regday = []
 
 contents.each do |row|
   id = row[0]
   name = row[:first_name]
   homephone = row[:homephone]
-  regtime = row[:regdate]
+  regdate = row[:regdate]
 
   phonenumber = clean_phone_number(homephone)
-  regtime = regtime.split(' ')[1]
+  regtime = regdate.split(' ')[1]
+  regday = regdate.split(' ')[0]
+  regday = peak_reg_day(regday)
+  temp_regday << regday
   peaktime = peak_time_reg(regtime)
   temp << peaktime
 
-  # zipcode = clean_zipcode(row[:zipcode])
-  # legislators = legislators_by_zipcode(zipcode)
+  zipcode = clean_zipcode(row[:zipcode])
+  legislators = legislators_by_zipcode(zipcode)
 
-  # form_letter = erb_template.result(binding)
-  # save_thank_you_letter(id, form_letter)
+  form_letter = erb_template.result(binding)
+  save_thank_you_letter(id, form_letter)
   #puts "#{id} #{name} #{phonenumber} #{regtime}"
 end
 
@@ -92,3 +101,11 @@ freq = temp.inject(Hash.new(0)) {|h,v| h[v]+= 1; h}
 max = freq.values.max
 most_registred_hours = Hash[freq.select { |k, v| v == max} ]
 puts "Most registered hours are #{most_registred_hours.keys.join(', ')}"
+
+
+freq_day = temp_regday.inject(Hash.new(0)) { |h,v| h[v] += 1; h }
+max = freq_day.values.max
+most_registred_day = Hash[freq_day.select { |k, v| v == max} ]
+day = most_registred_day.keys.join.to_i
+days_of_the_week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+puts "Most registered day is #{days_of_the_week[day]}"
